befe413d55d3c28ea90a8220355ef2250e480c42
Shogo1222 <kubotashogo@MacBook-Pro.local>
2020-07-22 19:23:12 +0900

    revert eslint

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -1,54 +1,62 @@
-require("dotenv").config()
-import colors from "vuetify/es5/util/colors"
+require('dotenv').config();
+import colors from 'vuetify/es5/util/colors'
 export default {
+
   watchers: {
     webpack: {
       poll: true
     }
   },
-  mode: "spa",
+  mode: 'spa',
   /*
-   ** Headers of the page
-   */
+  ** Headers of the page
+  */
   head: {
-    titleTemplate: "%s - " + process.env.npm_package_name,
-    title: process.env.npm_package_name || "",
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
-      }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
     //google maps apiのソースファイル
     script: [
       {
-        src: "https://maps.google.com/maps/api/js?key=" + process.env.API_KEY
+        src:
+          "https://maps.google.com/maps/api/js?key=" + process.env.API_KEY
       }
     ]
   },
   /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: "#fff" },
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
   /*
-   ** Global CSS
-   */
-  css: [],
+  ** Global CSS
+  */
+  css: [
+  ],
   /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: ["@nuxtjs/vuetify"],
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxtjs/vuetify',
+  ],
   plugins: [
-    "@/plugins/axios",
-    "@/plugins/vuetify",
+    '@/plugins/axios',
+    '@/plugins/vuetify',
     "@/plugins/auth-check",
     "@/plugins/vee-validate.js"
   ],
-  modules: ["@nuxtjs/axios", "@nuxtjs/proxy", "@nuxtjs/auth", "@nuxtjs/dotenv"],
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+    '@nuxtjs/auth',
+    '@nuxtjs/dotenv'
+  ],
   axios: {
     proxy: true
   },
@@ -60,15 +68,15 @@ export default {
   },
   auth: {
     redirect: {
-      login: "/login",
-      logout: "/login",
+      login: '/login',
+      logout: '/login',
       callback: false,
-      home: "/"
+      home: '/'
     },
     strategies: {
       local: {
         endpoints: {
-          login: { url: "/auth/sign_in", method: "post", propertyName: false },
+          login: { url: '/auth/sign_in', method: 'post', propertyName: false },
           logout: false,
           user: false
         }
@@ -76,11 +84,11 @@ export default {
     }
   },
   /*
-   ** vuetify module configuration
-   ** https://github.com/nuxt-community/vuetify-module
-   */
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
   vuetify: {
-    customVariables: ["~/assets/variables.scss"],
+    customVariables: ['~/assets/variables.scss'],
     theme: {
       dark: false,
       themes: {
@@ -97,12 +105,13 @@ export default {
     }
   },
   /*
-   ** Build configuration
-   */
+  ** Build configuration
+  */
   build: {
     /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {}
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
   }
 }
