65b6d9f1ebe6378359c802a657e5a3ae351318bc
Shogo1222 <kubotashogo@MacBook-Pro.local>
2020-07-25 20:35:40 +0900

    bugfix-comment-out

--- back/app/models/favorite.rb
+++ back/app/models/favorite.rb
@@ -1,7 +1,6 @@
 class Favorite < ApplicationRecord
   validates :user_id, presence: true
   validates :shop_id, presence: true
-  #validates :user_id, :shop_id, uniqueness: true
 
   has_many :user
 end
