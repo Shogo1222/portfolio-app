faa7d4fdc9d905e0e0a1d612db78ddbb5a2a3c3b
Shogo1222 <kubotashogo@MacBook-Pro.local>
2020-07-31 22:10:46 +0900

    config env status

--- front/plugins/auth-check.js
+++ front/plugins/auth-check.js
@@ -2,6 +2,9 @@ import firebase from "@/plugins/firebase"
 import axios from "@/plugins/axios"
 
 const authCheck = ({ store, redirect }) => {
+  console.log(process.env.API_KEY)
+  console.log(API_KEY)
+  console.log(${API_KEY})
   firebase.auth().onAuthStateChanged(async user => {
     if (user) {
       const { data } = await axios.get(`/v1/users?uid=${user.uid}`)
