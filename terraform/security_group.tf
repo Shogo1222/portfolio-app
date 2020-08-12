ffb1a8ebda396c5c6431f25bc5a2195dd06e5383
Shogo1222 <kubotashogo@MacBook-Pro.local>
2020-08-12 16:00:19 +0900

    fix sg for ecs and alb

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -22,10 +22,34 @@ resource "aws_security_group" "bischierge-front-security-group" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+      from_port       = 80
+      to_port         = 80
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+      ipv6_cidr_blocks     = ["::/0"]
+  }
+
+  ingress {
+      from_port       = 3000
+      to_port         = 3000
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+      ipv6_cidr_blocks     = ["::/0"]
+  }
+
   egress {
     from_port        = 3000
     to_port          = 3000
     protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
@@ -44,7 +68,6 @@ resource "aws_security_group" "bischierge-back-security-group" {
     to_port         = 3000
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
-    security_groups = [aws_security_group.bischierge-front-security-group.id]
     self            = false
   }
 
@@ -53,10 +76,9 @@ resource "aws_security_group" "bischierge-back-security-group" {
     to_port         = 443
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
-    security_groups = [aws_security_group.bischierge-front-security-group.id]
     self            = false
   }
-  
+
 }
 
 #######################
